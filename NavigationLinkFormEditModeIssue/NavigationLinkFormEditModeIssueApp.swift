53dfcae17ec29fb36911caef1f16a00c89581135
ProgressGuard <help@progressguard.com>
2022-05-15 10:36:18 +0300

    Initial Commit

---
+++ NavigationLinkFormEditModeIssue/NavigationLinkFormEditModeIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationLinkFormEditModeIssueApp.swift
+//  NavigationLinkFormEditModeIssue
+//
+//  Created by Gene Bogdanovich on 15.05.22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationLinkFormEditModeIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
